1b303d2bd4c3a06032d6b2d0fd344c1cc3dfa6d4
chad-livingston <chad.t.livingston.2@gmail.com>
2021-10-02 16:17:35 -0500

    some additions

--- xpense/src/xpense/xpense.java
+++ xpense/src/xpense/xpense.java
@@ -2,7 +2,7 @@ package xpense;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-//test second computer
+
 public class xpense {
 	public static ArrayList<Double> expenses = new ArrayList<Double>();
 	public static Scanner in = new Scanner(System.in);
@@ -13,33 +13,32 @@ public class xpense {
 		
 		while (running) {
 		//shows the user the available commands
-			String input = in.next();
+			String input = in.nextLine();
+			Double value = in.nextDouble();
 			if (input.equals("cmd")){
-				if (in.nextLine() == "cmd") {
 				System.out.println("\"add\" - use this too add an expense to the list of expenses.");
 				System.out.println("\"removelast\" - use this too remove the last added expense.");
 				System.out.println("\"view\" - use this too view all of your current expenses.");
 				System.out.println("\"reset\" - use this too reset your expense list entirely.");
 				System.out.println("\"sum\" - use this to add all of your expenses together.");
-				System.out.println("\"exit\" - use this to close the program.");
-				}			
+				System.out.println("\"exit\" - use this to close the program.");		
 			}
+			
+			//logic loop to add expenses until user confirms they are done
 			if (input.equals("add")) {
 				Boolean adding = false;
 				while (!adding) {
 					System.out.println("Please type your expense too add it too the expense list.");
 					if(in.hasNextDouble()) {
 						while(in.hasNextDouble()) {
-							addExpense(in.nextDouble());
+							addExpense(value);
 							System.out.println("Continue adding expenses or type \"done\" to quit.");
 							if (in.nextLine().toLowerCase().equals("done")) {
 								adding = true;
 							} else {
-								//System.out.println("You must enter either done or another expense in decimal format to continue.");
+								System.out.println("You must enter either done or another expense in decimal format to continue.");
 							}
 						}
-						//addExpense(in.nextDouble());
-						//System.out.println("Would you like to add another expense? y/n");
 
 						adding = true;
 						System.out.println("Now done adding expenses.");
@@ -49,33 +48,31 @@ public class xpense {
 					}
 				}
 			}
+			
+			//logic for removal of the last expense with user confirmation
 			if (input.equals("removelast")) {
-				Boolean removing = false;
-				while (!removing) {
 					System.out.println("Are you sure you would like to remove the last entered expense? y/n");
-					if (in.nextLine().toLowerCase().equals("y")) {
+					if (input.equals("y")) {
 						removeLast();
-						removing = true;
-					} else {
-						System.out.println("Aborting removal of last entered expense.");
-						removing = true;
-					}
+					} else if (input.equals("n")) {
+						System.out.println("Aborting removal of last entered expense.");					
 				}
 			}
+			//views the list of expenses.
 			if (input.equals("view")) {
 				System.out.println(expenses);
 			}
+			//resets expense list with user confirmation
 			if (in.nextLine().toLowerCase().equals("reset")) {
-				System.out.println("Are you sure you want to reset your entire expense list?  There is no restoring it after this. y/n");
-				Boolean resetting = false;
-				while (!resetting) {
-				if (in.nextLine().toLowerCase().equals("y")) {
+				System.out.println("Are you sure you want to reset your entire expense list?  There is no restoring it after this. y/n");				
+				if (input.equals("y")) {
 					expenses.clear();
 					System.out.println("All of your expenses have been cleared!");
+				} else if (input.equals("n")){
+					System.out.println("Aborting reset of expenses!");
 				}
 			}
-			}
-			
+			//logic for summing all expenses together
 			if (input.equals("sum")) {
 				if (expenses.size() != 0) {
 					double summed = sumExpenses();
@@ -86,9 +83,10 @@ public class xpense {
 					System.out.println("Your expenses are currently empty.");
 				}
 			}
+			//logic to close the program with user confirmation
 			if (input.equals("exit")) {
 				System.out.println("Are you sure you want to close the program? y/n");
-				if (in.nextLine().toLowerCase().equals("y")) {
+				if (input.equals("y")) {
 					System.out.println("Closing the program.");
 					in.close();
 					running = false;
@@ -97,15 +95,18 @@ public class xpense {
 			}
 		}	
 		}
+	//method to add expenses to the expense list
 		public static void addExpense(double expense) {
 			expenses.add(expense);
 			System.out.println("Adding " + expense + " to the list of expenses.");
 		}
+		//method to remove the last expense added from the expense list.
 		public static void removeLast() {
 			double removal = expenses.get(expenses.size() - 1);
 			expenses.remove(expenses.size() - 1);
 			System.out.println("Removed " + removal + " from the list of expenses");
 		}
+		//method to sum all expenses from the expense list
 		public static double sumExpenses() {
 			double sum = 0;
 			for (int i = 0; i < expenses.size(); i++) {
